e94669ccdc35a6d56eed9dd726b088bc3edbdb43
Aryansh <500093038@stu.upes.ac.in>
2022-10-09 22:05:08 +0530

    floyd-warshall algoraithm in python

---
+++ Algorithms/dynamic programming/floyd_warshall.py
@@ -0,0 +1,63 @@
+# Python Program for Floyd Warshall Algorithm
+
+# Number of vertices in the graph
+V = 4
+
+# Define infinity as the large
+
+INF = 99999
+
+
+
+
+def floydWarshall(graph):
+	""" dist[][] will be the output
+	matrix that will finally
+		have the shortest distances
+		between every pair of vertices """
+	
+
+	dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
+
+	
+	for k in range(V):
+
+		# pick all vertices as source one by one
+		for i in range(V):
+
+			
+			for j in range(V):
+
+				
+				dist[i][j] = min(dist[i][j],
+								dist[i][k] + dist[k][j]
+								)
+	printSolution(dist)
+
+
+
+def printSolution(dist):
+	print("Following matrix shows the shortest distances\
+between every pair of vertices")
+	for i in range(V):
+		for j in range(V):
+			if(dist[i][j] == INF):
+				print("%7s" % ("INF"), end=" ")
+			else:
+				print("%7d\t" % (dist[i][j]), end=' ')
+			if j == V-1:
+				print()
+
+
+
+if __name__ == "__main__":
+
+graph = [[0, 5, INF, 10],
+		[INF, 0, 3, INF],
+		[INF, INF, 0, 1],
+		[INF, INF, INF, 0]
+		]
+# Function call
+floydWarshall(graph)
+
+
